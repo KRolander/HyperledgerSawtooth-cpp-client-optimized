5eb7b9c909c0284bc07ba7ab2c88667f181b72d8
Roland <roland.kromes@etu.univ-cotedazur.fr>
2021-05-28 14:58:51 +0200

    Secp256k1 cryptographic library tools (ECDSA)

---
+++ secp256k1/src/basic-config.h
@@ -0,0 +1,20 @@
+/**********************************************************************
+ * Copyright (c) 2013, 2014 Pieter Wuille                             *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef SECP256K1_BASIC_CONFIG_H
+#define SECP256K1_BASIC_CONFIG_H
+
+#ifd
+
+#define USE_NUM_NONE 1
+#define USE_FIELD_INV_BUILTIN 1
+#define USE_SCALAR_INV_BUILTIN 1
+#define USE_FIELD_10X26 1
+#define USE_SCALAR_8X32 1
+#define ECMULT_WINDOW_SIZE 15
+#define USE_NUM_GMP 0
+
+#endif /* SECP256K1_BASIC_CONFIG_H */
